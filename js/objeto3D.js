d2c92ad5aae21c060fcbc447ff5d36f44951cc60
Luciano <lucianortiz97@gmail.com>
2021-11-01 19:47:59 -0300

    agrego clase objetoCurva3D para visualizacion de curvas

--- js/objeto3D.js
+++ js/objeto3D.js
@@ -1,62 +1,67 @@
 var modo = "smooth"
-function Objeto3D (){
+class Objeto3D {
     // atributos privados
-    var vertexBuffer = null; // position, normal & uvs
-    var indexBuffer = null;
-    var matModelado = mat4.create(); // transformacion respecto de su padre
-    var posicion = vec3.create(); //a partir de estos atributos se calcula la matriz respectiva
-    var rotacion = vec3.create();
-    var escala = vec3.create();
-    var color = vec3.fromValues(1,1,1);
-    var hijos=[];
+    constructor(){
+
+        this.vertexBuffer = null; // position, normal & uvs
+        this.indexBuffer = null;
+        this.matModelado = mat4.create(); // transformacion respecto de su padre
+        this.posicion = vec3.fromValues(0,0,0); //a partir de estos atributos se calcula la matriz respectiva
+        this.rotacion = vec3.fromValues(0,0,0);
+        this.escala = vec3.fromValues(1,1,1);
+        this.color = vec3.fromValues(1,1,1);
+        this.hijos=[];
+
+    }
+    
 
     // metodo privado, usa posicion, rotacion y escala. Se actualiza cada vez que se dibuja el objeto
-    function actualizarMatrizModelado() {
-        m = mat4.create(); // crear una matriz identidad
+    _actualizarMatrizModelado() {
+        let m = mat4.create(); // crear una matriz identidad
 
-        mat4.translate(m,m, posicion);
-        mat4.rotate(m,m,Math.PI/2,rotacion);
+        mat4.translate(m,m, this.posicion);
+        mat4.rotate(m,m,Math.PI/2,this.rotacion);
         //console.log(rotacion)
-        mat4.scale(m,m, escala);
-        matModelado = m;
+        mat4.scale(m,m, this.escala);
+        this.matModelado = m;
         //console.log(matModelado)
     } 
 
     // metodos publicos
-    this.dibujar = function(matPadre) { // (matPadre es la matriz identidad si es el objeto principal de la escena)
-        actualizarMatrizModelado();
+    dibujar(matPadre) { // (matPadre es la matriz identidad si es el objeto principal de la escena)
+        this._actualizarMatrizModelado();
         var m = mat4.create();
 
         // concatenamos las transformaciones padre/hijo
-        mat4.multiply(m, matPadre, matModelado);
+        mat4.multiply(m, matPadre, this.matModelado);
 
         var modelMatrixUniform = gl.getUniformLocation(glProgram, "modelMatrix");
         gl.uniformMatrix4fv(modelMatrixUniform, false, m);
 
-        if (vertexBuffer && indexBuffer) {
+        if (this.vertexBuffer && this.indexBuffer) {
             // Dibujamos la malla de triangulos con WebGL
             // si el objeto tiene geometria asociada.
             // Se configuran los buffers que alimentaron el pipeline
 
             vertexPositionAttribute = gl.getAttribLocation(glProgram, "aVertexPosition");
             gl.enableVertexAttribArray(vertexPositionAttribute);
-            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer.webgl_position_buffer);
-            gl.vertexAttribPointer(vertexPositionAttribute, vertexBuffer.webgl_position_buffer.itemSize, gl.FLOAT, false, 0, 0);
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer.webgl_position_buffer);
+            gl.vertexAttribPointer(vertexPositionAttribute, this.vertexBuffer.webgl_position_buffer.itemSize, gl.FLOAT, false, 0, 0);
 
             //gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer.webgl_uvs_buffer);
             //gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, vertexBuffer.webgl_uvs_buffer.itemSize, gl.FLOAT, false, 0, 0);
 
             vertexNormalAttribute = gl.getAttribLocation(glProgram, "aVertexNormal");
             gl.enableVertexAttribArray(vertexNormalAttribute);
-            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer.webgl_normal_buffer);
-            gl.vertexAttribPointer(vertexNormalAttribute, vertexBuffer.webgl_normal_buffer.itemSize, gl.FLOAT, false, 0, 0);
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer.webgl_normal_buffer);
+            gl.vertexAttribPointer(vertexNormalAttribute, this.vertexBuffer.webgl_normal_buffer.itemSize, gl.FLOAT, false, 0, 0);
             
-            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
 
-            difuseColorUniform = gl.getUniformLocation(glProgram, "difuseColor")
-            gl.uniform3f(difuseColorUniform, color[0],color[1], color[2])
+            let difuseColorUniform = gl.getUniformLocation(glProgram, "difuseColor")
+            gl.uniform3f(difuseColorUniform, this.color[0],this.color[1], this.color[2])
             
-            gl.drawElements( gl.TRIANGLE_STRIP, indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+            gl.drawElements( gl.TRIANGLE_STRIP, this.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 
 
             /*if (modo!="wireframe"){
@@ -70,41 +75,41 @@ function Objeto3D (){
             }*/
         }
 
-        for (var i = 0; i < hijos.length; i++) {
-            hijos[i].dibujar(m)
+        for (var i = 0; i < this.hijos.length; i++) {
+            this.hijos[i].dibujar(m)
         }
     }
 
-    this.setGeometria = function(newVertexBuffer, newIndexBuffer) {
-        vertexBuffer = newVertexBuffer;
-        indexBuffer = newIndexBuffer;
+    setGeometria(newVertexBuffer, newIndexBuffer) {
+        this.vertexBuffer = newVertexBuffer;
+        this.indexBuffer = newIndexBuffer;
     }
 
-    this.agregarHijo = function(h) {
-        hijos.push(h);
+    agregarHijo = function(h) {
+        this.hijos.push(h);
     }
-    this.quitarHijo = function(h) {
-        index = hijos.indexOf(h);
+    quitarHijo = function(h) {
+        index = this.hijos.indexOf(h);
         if (index > -1) {
-            hijos.splice(index, 1);
+            this.hijos.splice(index, 1);
         }
     }
 
-    this.setPosicion = function (x,y,z) {
-        posicion = vec3.fromValues(x,y,z)
+    setPosicion = function (x,y,z) {
+        this.posicion = vec3.fromValues(x,y,z)
     }
-    this.setRotacion = function(x,y,z) {
-        rotacion = vec3.fromValues(x,y,z)
+    setRotacion = function(x,y,z) {
+        this.rotacion = vec3.fromValues(x,y,z)
     }
-    this.setEscala = function (x,y,z) {
-        escala = vec3.fromValues(x,y,z)
-    }
-
-    this.setColor = function (r,g,b) {
-        color = vec3.fromValues(r,g,b)
+    setEscala = function (x,y,z) {
+        this.escala = vec3.fromValues(x,y,z)
     }
 
-    this.animate = function() {
+    setColor = function (r,g,b) {
+        this.color = vec3.fromValues(r,g,b)
+    }
+
+    animate = function() {
                 
         rotate_angle += 0.01;
         //mat4.identity(matModelado);
@@ -118,4 +123,44 @@ function Objeto3D (){
         mat4.transpose(normalMatrix,normalMatrix);
 
     }
+}
+
+
+class ObjetoCurva3D extends Objeto3D{
+    constructor(){
+        super()
+    }
+
+    dibujar(matPadre) {
+        this._actualizarMatrizModelado();
+        var m = mat4.create();
+
+        // concatenamos las transformaciones padre/hijo
+        mat4.multiply(m, matPadre, this.matModelado);
+
+        var modelMatrixUniform = gl.getUniformLocation(glProgram, "modelMatrix");
+        gl.uniformMatrix4fv(modelMatrixUniform, false, m);
+
+        if (this.vertexBuffer && this.indexBuffer) {
+            // Dibuja cada segmento de linea con WEBGL
+
+            vertexPositionAttribute = gl.getAttribLocation(glProgram, "aVertexPosition");
+            gl.enableVertexAttribArray(vertexPositionAttribute);
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer.webgl_position_buffer);
+            gl.vertexAttribPointer(vertexPositionAttribute, this.vertexBuffer.webgl_position_buffer.itemSize, gl.FLOAT, false, 0, 0);
+
+            vertexNormalAttribute = gl.getAttribLocation(glProgram, "aVertexNormal");
+            gl.enableVertexAttribArray(vertexNormalAttribute);
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer.webgl_normal_buffer);
+            gl.vertexAttribPointer(vertexNormalAttribute, this.vertexBuffer.webgl_normal_buffer.itemSize, gl.FLOAT, false, 0, 0);
+            
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+            let difuseColorUniform = gl.getUniformLocation(glProgram, "difuseColor")
+            gl.uniform3f(difuseColorUniform, this.color[0],this.color[1], this.color[2])
+            
+            gl.drawElements( gl.LINES, this.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+        }
+    }
+
 }
